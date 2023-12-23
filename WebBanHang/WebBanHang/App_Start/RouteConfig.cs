67252a3d0afbd19e8ea7b03672edeb5659ef755d
chien67 <95527965+chien67@users.noreply.github.com>
2023-12-23 15:50:05 +0700

    DB+ Areas, AdminLTE3
    Upload DB and AdminLTE3

--- WebBanHang/WebBanHang/App_Start/RouteConfig.cs
+++ WebBanHang/WebBanHang/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace WebBanHang
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "WebBanHang.Controllers" }
             );
         }
     }
--- WebBanHang/WebBanHang/Areas/Admin/AdminAreaRegistration.cs
+++ WebBanHang/WebBanHang/Areas/Admin/AdminAreaRegistration.cs
@@ -17,7 +17,8 @@ namespace WebBanHang.Areas.Admin
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
+                new { action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "WebBanHang.Areas.Admin.Controllers" }
             );
         }
     }
