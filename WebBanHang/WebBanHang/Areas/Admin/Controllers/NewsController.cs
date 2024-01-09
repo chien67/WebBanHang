10b5d02518107f6fe049ad396e646eec2763be62
chien67 <95527965+chien67@users.noreply.github.com>
2024-01-09 22:27:57 +0700

    add New

--- WebBanHang/WebBanHang/Areas/Admin/Controllers/NewsController.cs
+++ WebBanHang/WebBanHang/Areas/Admin/Controllers/NewsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebBanHang.Models;
+using WebBanHang.Models.EF;
 
 namespace WebBanHang.Areas.Admin.Controllers
 {
@@ -13,12 +14,50 @@ namespace WebBanHang.Areas.Admin.Controllers
         // GET: Admin/News
         public ActionResult Index()
         {
-            var item= db.News.OrderByDescending(x=>x.Id).ToList();
-            return View();
+            var items = db.News.OrderByDescending(x=>x.Id).ToList();
+            return View(items);
         }
         public ActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(New model)
+        {
+            if(ModelState.IsValid)
+            {
+                model.CreatedDate= DateTime.Now;
+                model.CategoryId = 1;
+                model.ModifiedDate= DateTime.Now;
+                model.Alias= WebBanHang.Models.Common.Filter.FilterChar(model.Title);
+                db.News.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(New model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.CreatedDate = DateTime.Now;
+                model.CategoryId = 1;
+                model.ModifiedDate = DateTime.Now;
+                model.Alias = WebBanHang.Models.Common.Filter.FilterChar(model.Title);
+                db.News.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
         }
     }
 }
---
+++ WebBanHang/WebBanHang/Migrations/202401091157147_UpdateDB.cs
@@ -0,0 +1,18 @@
+﻿namespace WebBanHang.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateDB : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.New", "Title", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.New", "Title", c => c.String());
+        }
+    }
+}
